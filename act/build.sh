5a604451e459d3b17aab09cb6276d289d77b75e6
defrostediceman <78688595+defrostediceman@users.noreply.github.com>
2023-05-21 22:10:10 +0000

    Act_runner refactor
    Dockerfile
    Changed to debian:stable-slim
    Removed golang & git from apt install
    Created user & directory
    Installed act_runner directly from binary
    
    Docker-compose
    Added build function
    Added act_runner config
    Changed environment variables to enable more configuration
    
    build.sh
    Removed due to included function in docker compose
    
    entrypoint.sh
    Refactored variables to enable more configuration

--- act/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build -t act_runner .
--- act/entrypoint.sh
+++ act/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./act_runner register --instance http://$SERVER_ADDR --token $TOKEN --labels $LABELS --no-interactive
+./act_runner register --instance $PROTOCOL://$FORGEJO_IP:$FORGEJO_PORT --token $TOKEN --labels $LABELS --no-interactive
 ./act_runner daemon
